31af9b10b0c5eb0f5a75fa1de575bc1b73330c5e
PixiePilot <89778100+PixiePilot@users.noreply.github.com>
2021-09-10 09:42:56 +0200

    Add files via upload

---
+++ feestlunch2.py
@@ -0,0 +1,4 @@
+bedrag = 18,88
+
+Factuurtekst = (' De feestlunch kost je bij de bakker ') + (str)bedrag + (' euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn! ')
+print(Factuurtekst)
---
+++ speel-hal-dag2.py
@@ -0,0 +1,3 @@
+bedrag = 44.44
+text = 'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar' + str(bedrag) + 'euro'
+print(text)
